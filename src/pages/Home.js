534659b236fe7fb54a76f6b6e8a4399ae5f06eb7
adarraji <a.darraji@outlook.com>
2022-08-19 23:55:20 -0400

    added Navbar component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Announcement from "../components/Announcement/Announcement";
+import Navbar from "../components/Navbar/Navbar";
 
 const Home = () => {
     return (
-        <Announcement />
+        <div>
+            <Announcement />
+            <Navbar />
+        </div>
     )
 }
 
