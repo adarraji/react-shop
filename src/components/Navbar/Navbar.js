e40ef9a4e3f0cebedc5e7cf26fbf0c995f8d0481
adarraji <a.darraji@outlook.com>
2022-08-20 00:12:26 -0400

    updated styles

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./Navbar.css";
+import Search from "@mui/icons-material/Search";
 
 const Navbar = () => {
     return (
         <div className="bg-light-gray h3 pa1 flex justify-between items-center">
-            <div className="flex-1">
+            <div className="flex-1 flex items-center">
                 <div className="f5 pointer">
                     {"EN"}
                 </div>
                 <div>
                     <input />
+                    <Search />
                 </div>
             </div>
             <div className="flex-1">center</div>
