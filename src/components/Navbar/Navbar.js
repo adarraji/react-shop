5ac1c24994cd3b00496c7ad18a4ea84e4e52b213
adarraji <a.darraji@outlook.com>
2022-08-20 00:01:11 -0400

    added Navbar sections and more styles

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,11 @@ import "./Navbar.css";
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className="bg-light-gray h3 pa1 flex justify-between items-center">
+            <div>left</div>
+            <div>center</div>
+            <div>right</div>
+        </div>
     )
 }
 
