2ed61efc39fb58116646f6e2ed9a716672c6cd99
techbrain2 <techbrain203@outlook.com>
2022-08-20 14:33:54 -0400

    imports

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./Slider.css";
+import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import { sliderItems } from "../../data";
 
 const Slider = () => {
     return (
