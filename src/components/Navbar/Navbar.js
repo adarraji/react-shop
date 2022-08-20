1ae807dc22bd3f17b1c37607cda742954d3fa147
adarraji <a.darraji@outlook.com>
2022-08-20 00:18:05 -0400

    updated styles

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,12 +4,12 @@ import Search from "@mui/icons-material/Search";
 
 const Navbar = () => {
     return (
-        <div className="bg-light-gray h3 pa1 flex justify-between items-center">
+        <div className="bg-light-gray h3 pv2 ph3 flex justify-between items-center">
             <div className="flex-1 flex items-center">
                 <div className="f5 pointer">
                     {"EN"}
                 </div>
-                <div>
+                <div className="ba b--light-gray flex items-center ml3 pa">
                     <input />
                     <Search />
                 </div>
