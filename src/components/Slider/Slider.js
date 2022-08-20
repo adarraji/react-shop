88f5467ca9feba6f74cbe6003770c86a86551175
techbrain2 <techbrain203@outlook.com>
2022-08-20 14:55:40 -0400

    added container and arrow styles

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -6,12 +6,12 @@ import { sliderItems } from "../../data";
 
 const Slider = () => {
     return (
-        <div>
-            <div>
+        <div className="w-100 vh-100 flex">
+            <div className="slider-arrow w2 h2">
                 <ArrowLeftIcon />
             </div>
             <div>WRAPPER</div>
-            <div>
+            <div className="slider-arrow w2 h2">
                 <ArrowRightIcon />
             </div>
         </div>
