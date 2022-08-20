c658c058bfd83b629dada9ab2539972e9a754088
adarraji <a.darraji@outlook.com>
2022-08-20 00:04:24 -0400

    added language and search container

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,12 @@ import "./Navbar.css";
 const Navbar = () => {
     return (
         <div className="bg-light-gray h3 pa1 flex justify-between items-center">
-            <div className="flex-1">left</div>
+            <div className="flex-1">
+                <div>{"EN"}</div>
+                <div>
+                    <input />
+                </div>
+            </div>
             <div className="flex-1">center</div>
             <div className="flex-1">right</div>
         </div>
