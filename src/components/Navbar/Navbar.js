a326e5d04730745a8ffdea99bbf69580640beb35
adarraji <a.darraji@outlook.com>
2022-08-20 00:02:29 -0400

    added flex-1 to 3 sections

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,9 +4,9 @@ import "./Navbar.css";
 const Navbar = () => {
     return (
         <div className="bg-light-gray h3 pa1 flex justify-between items-center">
-            <div>left</div>
-            <div>center</div>
-            <div>right</div>
+            <div className="flex-1">left</div>
+            <div className="flex-1">center</div>
+            <div className="flex-1">right</div>
         </div>
     )
 }
