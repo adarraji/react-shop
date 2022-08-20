2385b4e609e7f3216a17ad41b99b8d9d54fd41b5
techbrain2 <techbrain203@outlook.com>
2022-08-20 14:30:33 -0400

    added slider component

---
+++ src/components/Slider/Slider.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Slider.css";
+
+const Slider = () => {
+    return (
+        <div>slider</div>
+    )
+}
+
+export default Slider
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Announcement from "../components/Announcement/Announcement";
 import Navbar from "../components/Navbar/Navbar";
+import Slider from "../components/Slider/Slider";
 
 const Home = () => {
     return (
         <div>
             <Announcement />
             <Navbar />
+            <Slider />
         </div>
     )
 }
