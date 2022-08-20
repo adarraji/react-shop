65bfa14544321e3fe8d81fa97926a88a359711ad
adarraji <a.darraji@outlook.com>
2022-08-19 21:11:23 -0400

    added home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
-import './App.css';
+import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Home />
     </div>
   );
 }
