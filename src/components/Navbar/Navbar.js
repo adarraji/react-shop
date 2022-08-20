0e1de89892d1ff827f1effcafa280e633a27719f
adarraji <a.darraji@outlook.com>
2022-08-20 00:22:31 -0400

    added styles to input and search

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,14 +4,14 @@ import Search from "@mui/icons-material/Search";
 
 const Navbar = () => {
     return (
-        <div className="bg-light-gray h3 pv2 ph3 flex justify-between items-center">
+        <div className="h3 pv2 ph3 flex justify-between items-center">
             <div className="flex-1 flex items-center">
                 <div className="f5 pointer">
                     {"EN"}
                 </div>
-                <div className="ba b--light-gray flex items-center ml3 pa">
-                    <input />
-                    <Search />
+                <div className="ba b--light-silver flex items-center ml3 pa">
+                    <input className="border-none" />
+                    <Search className="gray" style={{ fontSize: 16 }} />
                 </div>
             </div>
             <div className="flex-1">center</div>
