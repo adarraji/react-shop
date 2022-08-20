698fc71f8bf330932c63e059bf994ede0a6a13a8
techbrain2 <techbrain203@outlook.com>
2022-08-20 13:45:30 -0400

    changed background color

--- src/components/Announcement/Announcement.js
+++ src/components/Announcement/Announcement.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Announcement.css"
 
 const Announcement = () => {
     return (
-        <div className="h2 f4 fw5 bg-dark-green white flex justify-center items-center ">
+        <div className="h2 f4 fw5 bg-dark-green white flex justify-center items-center announce-container">
             {"Super Deal! Free Shipping on Orders Over 50%"}
         </div>
     )
