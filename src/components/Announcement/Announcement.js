5069e54d08f5659756eeec97bf24bc91765470c3
adarraji <a.darraji@outlook.com>
2022-08-19 23:52:34 -0400

    updated styles

--- src/components/Announcement/Announcement.js
+++ src/components/Announcement/Announcement.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Announcement = () => {
     return (
-        <div className="f3 fw-5 bg-dark-green white flex justify-center items-center ">
+        <div className="h2 f4 fw5 bg-dark-green white flex justify-center items-center ">
             {"Super Deal! Free Shipping on Orders Over 50%"}
         </div>
     )
