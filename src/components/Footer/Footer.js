624e06e740189a3470a2889d7fe928537e732ff0
techbrain2 <techbrain203@outlook.com>
2022-08-21 11:06:20 -0400

    LEFT header and desciption

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,11 @@ import "./Footer.css";
 const Footer = () => {
     return (
         <div className="flex">
-            <div className="flex-1 pa3">LEFT</div>
+            <div className="flex-1 pa3">
+                <h1>Ecommerce Store</h1>
+                <div>{"There are many variations of passages of Lorem Ipsum available, but The majority have suffered alteration in some form, by injected humour, or randomised words which don’t look even slightly believable."}</div>
+                <div></div>
+            </div>
             <div className="flex-1 pa3">CENTER</div>
             <div className="flex-1 pa3">RIGHT</div>
         </div>
