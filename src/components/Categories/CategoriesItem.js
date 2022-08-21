75ab21bbd09b83b1ce1f1da05e69d126bf693c5c
techbrain2 <techbrain203@outlook.com>
2022-08-21 09:48:41 -0400

    button styles

--- src/components/Categories/CategoriesItem.js
+++ src/components/Categories/CategoriesItem.js
@@ -7,7 +7,7 @@ const CategoriesItem = ({ item }) => {
             <img src={item.img} className="h-100 w-100" />
             <div className="absolute top-0 left-0 w-100 h-100 flex flex-column justify-center items-center">
                 <h1 className="white ma3">{item.title}</h1>
-                <button>SHOP NOW</button>
+                <button className="pa2 bg-white gray pointer fw6">SHOP NOW</button>
             </div>
         </div>
     )
