8005c01e0d272a0e4b777ab5a0d4df88d450fa6d
techbrain2 <techbrain203@outlook.com>
2022-08-21 12:47:08 -0400

    replace Home with Productist

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import "./App.css";
 import Home from "./pages/Home/Home";
+import ProductList from "./pages/ProductList/ProductList";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <ProductList />
     </div>
   );
 }
