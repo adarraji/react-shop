a20ccc898600f41da182b64608547eaed31b9e3f
techbrain2 <techbrain203@outlook.com>
2022-08-21 10:07:12 -0400

    added item image

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Product.css";
 
-const Product = () => {
+const Product = ({ item }) => {
     return (
-        <div>
-            <div></div>
-            <img />
+        <div className="product relative">
+            <div className="w-50 h-50 bg-white absolute"></div>
+            <img src={item.img} />
             <div></div>
         </div>
     )
