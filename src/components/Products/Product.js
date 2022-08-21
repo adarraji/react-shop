2b42bf08e0322d2072ae55ae18ecf38ae9084f9f
techbrain2 <techbrain203@outlook.com>
2022-08-21 09:51:33 -0400

    products component

---
+++ src/components/Products/Product.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Product.css";
+
+const Product = () => {
+    return (
+        <div>Product</div>
+    )
+}
+
+export default Product
---
+++ src/components/Products/Products.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Product.css";
+
+const Products = () => {
+    return (
+        <div>Products</div>
+    )
+}
+
+export default Products
