201bb8cc5501c6ed0f936feec62a040bb2d12a11
techbrain2 <techbrain203@outlook.com>
2022-08-21 09:44:25 -0400

    info styles

--- src/components/Categories/CategoriesItem.js
+++ src/components/Categories/CategoriesItem.js
@@ -5,7 +5,7 @@ const CategoriesItem = ({ item }) => {
     return (
         <div className="flex-1 ma1 vh-75 relative">
             <img src={item.img} className="h-100 w-100" />
-            <div>
+            <div className="absolute top-0 left-0 w-100 h-100 flex flex-column justify-center items-center">
                 <h1>{item.title}</h1>
                 <button>SHOP NOW</button>
             </div>
