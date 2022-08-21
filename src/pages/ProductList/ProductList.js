7e70c0fed8d1dc2057dc68ed47adc9bc19af2fe3
adarraji <a.darraji@outlook.com>
2022-08-21 18:07:29 -0400

    added css file

--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./ProductList.css";
 import Announcement from "../../components/Announcement/Announcement";
 import Navbar from "../../components/Navbar/Navbar";
 
