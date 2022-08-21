bde6f1188cfdd76b272d81d08ac8c0b7a45a6cc4
techbrain2 <techbrain203@outlook.com>
2022-08-21 09:02:48 -0400

    added arrow clicker handler

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Slider.css";
 import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 import { sliderItems } from "../../data";
 
 const Slider = () => {
+    const [slideIndex, setSlideIndex] = useState(0);
+    const onArrowClick = (direction) => {
+        if (direction === "left") {
+            setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+        } else if (direction === "right") {
+            setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0)
+        }
+    }
     return (
         <div className="w-100 vh-100 flex relative overflow-hidden">
-            <div className="slider-arrow w2 h2 flex justify-center absolute top-0 bottom-0 pointer o-50 z-2 left-1">
+            <div onClick={() => onArrowClick("left")} className="slider-arrow w2 h2 flex justify-center absolute top-0 bottom-0 pointer o-50 z-2 left-1">
                 <ArrowLeftIcon />
             </div>
             <div className="wrapper h-100 flex justify-center ">
@@ -26,7 +34,7 @@ const Slider = () => {
                     ))
                 }
             </div>
-            <div className="slider-arrow w2 h2 flex justify-center absolute top-0 bottom-0 pointer o-50 z-2 right-1">
+            <div onClick={() => onArrowClick("right")} className="slider-arrow w2 h2 flex justify-center absolute top-0 bottom-0 pointer o-50 z-2 right-1">
                 <ArrowRightIcon />
             </div>
         </div>
