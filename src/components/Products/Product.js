29ee59526c98f81f37188f910f027336a21bfc00
techbrain2 <techbrain203@outlook.com>
2022-08-21 10:16:44 -0400

    added product icons

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -1,12 +1,25 @@
 import React from "react";
 import "./Product.css";
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 
 const Product = ({ item }) => {
     return (
         <div className="product relative flex justify-center items-center ma1">
             <div className="circle bg-white absolute"></div>
             <img className="h-75 z-2" src={item.img} />
-            <div></div>
+            <div>
+                <div>
+                    <ShoppingCartOutlinedIcon />
+                </div>
+                <div>
+                    <SearchOutlinedIcon />
+                </div>
+                <div>
+                    <FavoriteBorderOutlinedIcon />
+                </div>
+            </div>
         </div>
     )
 }
