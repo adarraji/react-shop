8c308de968a9f887f86fbca56a5e4cb9eaf5bc4b
techbrain2 <techbrain203@outlook.com>
2022-08-21 11:11:52 -0400

    LEFT styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,9 +4,9 @@ import "./Footer.css";
 const Footer = () => {
     return (
         <div className="flex">
-            <div className="flex-1 pa3">
-                <h1>Ecommerce Store</h1>
-                <div>{"There are many variations of passages of Lorem Ipsum available, but The majority have suffered alteration in some form, by injected humour, or randomised words which don’t look even slightly believable."}</div>
+            <div className="flex-1 pa3 flex flex-column tl">
+                <h1 className="pl3">Ecommerce Store</h1>
+                <div className="mt2 mb2 pl3">{"There are many variations of passages of Lorem Ipsum available, but The majority have suffered alteration in some form, by injected humour, or randomised words which don’t look even slightly believable."}</div>
                 <div></div>
             </div>
             <div className="flex-1 pa3">CENTER</div>
