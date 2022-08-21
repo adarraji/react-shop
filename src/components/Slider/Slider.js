6e71f105e5477b32785d7fb50e437a7d49e64a4f
techbrain2 <techbrain203@outlook.com>
2022-08-21 08:44:42 -0400

    updated styles

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -10,14 +10,14 @@ const Slider = () => {
             <div className="slider-arrow w2 h2 flex justify-center absolute top-0 bottom-0 pointer o-50 z-2 left-1">
                 <ArrowLeftIcon />
             </div>
-            <div className="h-100 flex">
+            <div className="h-100 flex justify-center">
                 {
                     sliderItems.map(item => (
-                        <div key={item.id} className="slide flex items-center vh-100">
+                        <div key={item.id} className="slide flex justify-center items-center vh-100">
                             <div className="flex-1 h-100 pl5 flex justify-end items-center">
                                 <img src={item.img} />
                             </div>
-                            <div className="flex-1">
+                            <div className="flex-1 pa6">
                                 <h1>{item.title}</h1>
                                 <p>{item.desc}</p>
                                 <button>SHOP NOW</button>
