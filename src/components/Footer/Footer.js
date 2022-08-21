b8b531c9659bfd563bd47a1cb758d05299489d08
techbrain2 <techbrain203@outlook.com>
2022-08-21 11:16:48 -0400

    added social icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,30 @@
 import React from "react";
 import "./Footer.css";
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import PinterestIcon from '@mui/icons-material/Pinterest';
 
 const Footer = () => {
     return (
         <div className="flex">
             <div className="flex-1 pa3 flex flex-column tl">
                 <h1 className="pl3">Ecommerce Store</h1>
-                <div className="mt2 mb2 pl3">{"There are many variations of passages of Lorem Ipsum available, but The majority have suffered alteration in some form, by injected humour, or randomised words which don’t look even slightly believable."}</div>
-                <div></div>
+                <div className="ma0 mt2 mb2 pl3">{"There are many variations of passages of Lorem Ipsum available, but The majority have suffered alteration in some form, by injected humour, or randomised words which don’t look even slightly believable."}</div>
+                <div>
+                    <div>
+                        <FacebookIcon />
+                    </div>
+                    <div>
+                        <InstagramIcon />
+                    </div>
+                    <div>
+                        <TwitterIcon />
+                    </div>
+                    <div>
+                        <PinterestIcon />
+                    </div>
+                </div>
             </div>
             <div className="flex-1 pa3">CENTER</div>
             <div className="flex-1 pa3">RIGHT</div>
