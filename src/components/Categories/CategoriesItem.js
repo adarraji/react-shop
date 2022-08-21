90c90cc1cdce72abed8197d997b2d3bd305c51c8
techbrain2 <techbrain203@outlook.com>
2022-08-21 09:40:58 -0400

    image styles

--- src/components/Categories/CategoriesItem.js
+++ src/components/Categories/CategoriesItem.js
@@ -4,7 +4,7 @@ import "./CategoriesItem.css";
 const CategoriesItem = ({ item }) => {
     return (
         <div className="flex-1 ma1 vh-75 relative">
-            <img src={item.img} />
+            <img src={item.img} className="h-100 w-100" />
             <div>
                 <h1>{item.title}</h1>
                 <button>SHOP NOW</button>
