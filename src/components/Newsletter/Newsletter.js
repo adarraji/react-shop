c7869e4b84fe9c0063704602f1fa16171cea2f37
techbrain2 <techbrain203@outlook.com>
2022-08-21 10:53:22 -0400

    updated styles

--- src/components/Newsletter/Newsletter.js
+++ src/components/Newsletter/Newsletter.js
@@ -9,8 +9,8 @@ const Newsletter = () => {
             <h1 className="f1 mb3">Newsletter</h1>
             <div className="f3 fw5 mb3">Get timely updates from your favorite products.</div>
             <div className="input-container bg-white flex justify-between ba b--light-silver">
-                <input className="pa2" placeholder="Your email" />
-                <button className="white flex justify-center items-center">
+                <input className="pa2 br-none flex-8" placeholder="Your email" />
+                <button className="br-none flex-1 bg-teal white flex justify-center items-center">
                     <SendIcon />
                 </button>
             </div>
