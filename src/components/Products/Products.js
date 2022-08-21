b3f27de674fec0fb1679d66e4d8303b75b908f40
techbrain2 <techbrain203@outlook.com>
2022-08-21 09:56:25 -0400

    added products items

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,9 +1,17 @@
 import React from "react";
 import "./Product.css";
+import { popularProducts } from "../../data";
+import Product from "./Product";
 
 const Products = () => {
     return (
-        <div>Products</div>
+        <div className="pa4 flex justify-between flex-wrap">
+            {
+                popularProducts.map(item => (
+                    <Product item={item} key={item.id} />
+                ))
+            }
+        </div>
     )
 }
 
