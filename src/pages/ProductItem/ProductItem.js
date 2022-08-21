0b1101f7135af54c7b84ecd2385ea0ef3e94a505
adarraji <a.darraji@outlook.com>
2022-08-21 19:11:06 -0400

    added image, info, select divs

--- src/pages/ProductItem/ProductItem.js
+++ src/pages/ProductItem/ProductItem.js
@@ -11,6 +11,38 @@ const ProductItem = () => {
         <div>
             <Announcement />
             <Navbar />
+            <div>
+                <div>
+                    <img src="https://i.ibb.co/S6qMxwr/jean.jpg" />
+                </div>
+                <div>
+                    <h1>Denim Jumpsuit</h1>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                        venenatis, dolor in finibus malesuada, lectus ipsum porta nunc, at
+                        iaculis arcu nisi sed mauris. Nulla fermentum vestibulum ex, eget
+                        tristique tortor pretium ut. Curabitur elit justo, consequat id
+                        condimentum ac, volutpat ornare.</p>
+                    <span>$20</span>
+                    <div>
+                        <div>
+                            <span>Color</span>
+                            <div>black</div>
+                            <div>darkblue</div>
+                            <div>gray</div>
+                        </div>
+                        <div>
+                            <select name="size" id="size" defaultValue={"DEFAULT"}>
+                                <option value="DEFAULT" disabled>Size</option>
+                                <option value="xs">XS</option>
+                                <option value="s">S</option>
+                                <option value="m">M</option>
+                                <option value="l">L</option>
+                                <option value="xl">XL</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <Newsletter />
             <Footer />
         </div>
