8a14445f5ab458c859605e8f03117c2f067b6c24
adarraji <a.darraji@outlook.com>
2022-08-23 07:37:44 -0400

    created components

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,10 +1,21 @@
 import React from "react";
 import styled from "styled-components";
+import Announcement from "../components/Announcement/Announcement";
+import Navbar from "../components/Navbar/Navbar";
+import Newsletter from "../components/Newsletter/Newsletter";
+import Footer from "../components/Footer/Footer";
 
 const Cart = () => {
     return (
-        <div>Cart</div>
+        <Container>
+            <Announcement />
+            <Navbar />
+            CART
+            <Footer />
+        </Container>
     )
 }
+
+const Container = styled.div``;
 
 export default Cart
