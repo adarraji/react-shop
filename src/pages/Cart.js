57bb0634a2ca6cb2e60caecf4fcd396a6bd6703c
adarraji <a.darraji@outlook.com>
2022-08-23 07:41:35 -0400

    created more components

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -4,13 +4,18 @@ import Announcement from "../components/Announcement/Announcement";
 import Navbar from "../components/Navbar/Navbar";
 import Newsletter from "../components/Newsletter/Newsletter";
 import Footer from "../components/Footer/Footer";
+import { style } from "@mui/system";
 
 const Cart = () => {
     return (
         <Container>
             <Announcement />
             <Navbar />
-            CART
+            <Wrapper>
+                <Title>YOUR BAG</Title>
+                <Top></Top>
+                <Bottom></Bottom>
+            </Wrapper>
             <Footer />
         </Container>
     )
@@ -19,5 +24,13 @@ const Cart = () => {
 const Container = styled.div.attrs({
     className: "tl"
 })``;
+
+const Wrapper = styled.div``;
+
+const Title = styled.h1``;
+
+const Top = styled.div``;
+
+const Bottom = styled.div``;
 
 export default Cart
