739df32c7c4bc84b0dc81e7db406e25c43c2614c
adarraji <a.darraji@outlook.com>
2022-08-23 07:23:02 -0400

    added login to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
 import "./App.css";
-import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
-      <Register />
+      <Login />
     </div>
   );
 }
