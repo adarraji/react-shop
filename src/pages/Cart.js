012ecacceaf96d6b542748e8493c82ffd65db144
adarraji <a.darraji@outlook.com>
2022-08-24 17:46:32 -0400

    added hr line

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -48,7 +48,7 @@ const Cart = () => {
                                 <ProductPrice>$ 20</ProductPrice>
                             </PriceDetail>
                         </Product>
-                        <hr />
+                        <HR />
                         <Product>
                             <ProductDetail>
                                 <Image src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A"></Image>
@@ -119,6 +119,8 @@ const Bottom = styled.div.attrs({
 const Info = styled.div`
     flex:3;
 `;
+
+const HR = styled.hr``;
 
 const Summary = styled.div`
     flex:1;
