5b4f8908091924bd7d7411dfa8e86d774d899e05
adarraji <a.darraji@outlook.com>
2022-08-24 17:49:47 -0400

    changed  text and image for second product

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -51,17 +51,17 @@ const Cart = () => {
                         <HR />
                         <Product>
                             <ProductDetail>
-                                <Image src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A"></Image>
+                                <Image src="https://i.pinimg.com/originals/2d/af/f8/2daff8e0823e51dd752704a47d5b795c.png"></Image>
                                 <Details>
                                     <ProductName>
-                                        JESSIE THUNDER SHOES
+                                        HAKURA T-SHIRT
                                     </ProductName>
                                     <ProductId>
                                         <b>ID:</b> 93813718293
                                     </ProductId>
-                                    <ProductColor color="black" />
+                                    <ProductColor color="gray" />
                                     <ProductSize>
-                                        <b>Size:</b> 37.5
+                                        <b>Size:</b> M
                                     </ProductSize>
                                 </Details>
                             </ProductDetail>
