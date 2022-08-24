f0245edd906bac1b8a79818fa5d184d7057800cb
adarraji <a.darraji@outlook.com>
2022-08-24 18:17:17 -0400

    changec app to home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import "./App.css";
 import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+
 
 function App() {
   return (
     <div className="App">
-      <Cart />
+      <Home />
     </div>
   );
 }
