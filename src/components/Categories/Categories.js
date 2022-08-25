02d77ca83487f917edd719118a5ceefac91a7212
techbrain2 <techbrain203@outlook.com>
2022-08-25 18:51:51 -0400

    Added mobile responsive

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -2,6 +2,7 @@ import React from "react";
 import { categories } from "../../data";
 import CategoriesItem from "./CategoriesItem"
 import styled from "styled-components";
+import { mobile } from "../../responsive";
 
 
 const Categories = () => {
@@ -19,6 +20,9 @@ const Categories = () => {
 
 const Container = styled.div.attrs({
     className: "pa4 flex justify- between"
-})``;
+})`
+    ${mobile({ padding: "0px", flexDirection: "column" })}   
+
+`;
 
 export default Categories
