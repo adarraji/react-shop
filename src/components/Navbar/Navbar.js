27f8a85b61f1f49d10ad2a6155817a514d7f103f
adarraji <a.darraji@outlook.com>
2022-08-24 22:41:18 -0400

    Changed to styled-componenets

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,36 +1,88 @@
 import React from "react";
-import "./Navbar.css";
 import Search from "@mui/icons-material/Search";
 import Badge from "@mui/material/Badge";
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import styled from "styled-components";
 
 const Navbar = () => {
     return (
-        <div className="h3 pv2 ph3 flex justify-between items-center">
-            <div className="flex-1 flex items-center">
-                <div className="f5 pointer">
-                    {"EN"}
-                </div>
-                <div className="ba b--light-silver flex items-center ml3 pa">
-                    <input className="border-none" />
-                    <Search className="gray" style={{ fontSize: 16 }} />
-                </div>
-            </div>
-            <div className="flex-1">
-                <h1 className="tc">Ecommerce Store</h1>
-            </div>
-            <div className="flex-1 flex justify-end items-center">
-                <div className="f5 ml3 pointer">{"REGISTER"}</div>
-                <div className="f5 ml3 pointer">{"SIGNIN"}</div>
-                <div className="f5 ml3 pointer">
-                    <Badge badgeContent={4} color="primary">
-                        <ShoppingCartOutlinedIcon color="action" />
-                    </Badge>
-                </div>
-
-            </div>
-        </div>
+        <Container>
+            <Wrapper>
+                <Left>
+                    <Language>EN</Language>
+                    <SearchContainer>
+                        <Input></Input>
+                        <Search style={{ fontSize: 16 }} />
+                    </SearchContainer>
+                </Left>
+                <Center>
+                    <Logo>Ecommerce Store</Logo>
+                </Center>
+                <Right>
+                    <MenuItem>REGISTER</MenuItem>
+                    <MenuItem>SIGNIN</MenuItem>
+                    <MenuItem>
+                        <Badge>
+                            <ShoppingCartOutlinedIcon />
+                        </Badge>
+                    </MenuItem>
+                </Right>
+            </Wrapper>
+        </Container>
     )
 }
+
+const Container = styled.div`
+    height: 70px;
+`;
+
+const Wrapper = styled.div.attrs({
+    className: "flex justify-between items-center pv1 ph3"
+})``;
+
+
+const Left = styled.div.attrs({
+    className: "flex items-center"
+})`
+    flex: 1;
+`;
+
+
+const Language = styled.span.attrs({
+    className: "f5 pointer"
+})``;
+
+const SearchContainer = styled.div.attrs({
+    className: "ba b--light-gray flex items-center ml3 pa"
+})``;
+
+const Input = styled.input.attrs({
+    className: ""
+})`
+    border:none;
+`;
+
+const Center = styled.div.attrs({
+    className: ""
+})`
+    flex: 1;
+`;
+
+const Logo = styled.h1.attrs({
+    className: "tc"
+})``;
+
+
+
+const Right = styled.div.attrs({
+    className: "flex justify-end items-center"
+})`
+    flex: 1;
+`;
+
+
+const MenuItem = styled.div.attrs({
+    className: "f5 ml3 pointer"
+})``;
 
 export default Navbar
