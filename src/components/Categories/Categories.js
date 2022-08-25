ebcfe3b6b08c559ee95dfbb84e12a7b142707c5e
adarraji <a.darraji@outlook.com>
2022-08-25 15:22:21 -0400

    changed to styled components

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,18 +1,24 @@
 import React from "react";
 import { categories } from "../../data";
 import CategoriesItem from "./CategoriesItem"
+import styled from "styled-components";
 
 
 const Categories = () => {
     return (
-        <div className="pa4 flex justify-between">
+        <Container>
             {
                 categories.map(item => (
                     <CategoriesItem item={item} key={item.id} />
                 ))
             }
-        </div>
+        </Container>
     )
 }
+
+
+const Container = styled.div.attrs({
+    className: "pa4 flex justify- between"
+})``;
 
 export default Categories
--- src/components/Categories/CategoriesItem.js
+++ src/components/Categories/CategoriesItem.js
@@ -1,16 +1,42 @@
 import React from "react";
-import "./CategoriesItem.css";
+import styled from "styled-components";
 
 const CategoriesItem = ({ item }) => {
     return (
-        <div className="flex-1 ma1 vh-75 relative">
-            <img src={item.img} alt="category" className="h-100 w-100 fit-cover" />
-            <div className="absolute top-0 left-0 w-100 h-100 flex flex-column justify-center items-center">
-                <h1 className="white ma3">{item.title}</h1>
-                <button className="pa2 bg-white gray pointer fw6">SHOP NOW</button>
-            </div>
-        </div>
+        <Container>
+            <Image src={item.img} alt="category" />
+            <Info>
+                <Title>{item.title}</Title>
+                <Button>SHOP NOW</Button>
+            </Info>
+        </Container>
     )
 }
+
+const Container = styled.div.attrs({
+    className: "ma1 vh-75 relative"
+})`
+    flex: 1;
+`;
+
+const Image = styled.img.attrs({
+    className: "h-100 w-100"
+})`
+     object-fit: cover;
+`;
+
+const Info = styled.div.attrs({
+    className: "absolute top-0 left-0 w-100 h-100 flex flex-column justify-center items-center"
+})``;
+
+const Title = styled.h1.attrs({
+    className: "white mb3"
+})``;
+
+const Button = styled.button.attrs({
+    className: "pa2 fw6 bg-white gray"
+})`
+    border:none;
+`;
 
 export default CategoriesItem
