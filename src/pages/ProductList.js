123bf08be9b482ed6b18add6732e924848f0ee2a
techbrain2 <techbrain203@outlook.com>
2022-08-25 19:37:23 -0400

    Added mobile responsive

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -5,6 +5,8 @@ import Products from "../components/Products/Products";
 import Newsletter from "../components/Newsletter/Newsletter";
 import Footer from "../components/Footer/Footer";
 import styled from 'styled-components';
+import { mobile } from "../responsive";
+
 
 
 const ProductList = () => {
@@ -47,36 +49,6 @@ const ProductList = () => {
                     </Select>
                 </Filter>
             </FilterContainer>
-            {/* <div className="flex justify-between">
-                <div className="ma3">
-                    <span className="f5 fw6 mr3">Filter Products:</span>
-                    <select className="pa2 mr3" name="color" id="color" defaultValue={"DEFAULT"}>
-                        <option value="DEFAULT" disabled>Color</option>
-                        <option value="white">White</option>
-                        <option value="black">Black</option>
-                        <option value="red">Red</option>
-                        <option value="blue">Blue</option>
-                        <option value="yellow">Yellow</option>
-                        <option value="green">Green</option>
-                    </select>
-                    <select className="pa2 mr3" name="size" id="size" defaultValue={"DEFAULT"}>
-                        <option value="DEFAULT" disabled>Size</option>
-                        <option value="xs">XS</option>
-                        <option value="s">S</option>
-                        <option value="m">M</option>
-                        <option value="l">L</option>
-                        <option value="xl">XL</option>
-                    </select>
-                </div>
-                <div className="ma3">
-                    <span className="f5 fw6 mr3">Sort Products:</span>
-                    <select className="pa2 mr3" name="sort" id="sort" defaultValue={"newest"}>
-                        <option value="newest">Newest</option>
-                        <option value="price-asc">Price (asc)</option>
-                        <option value="prrice-desc">Price (desc)</option>
-                    </select>
-                </div>
-            </div> */}
             <Products />
             <Newsletter />
             <Footer />
@@ -96,15 +68,23 @@ const FilterContainer = styled.div.attrs({
 
 const Filter = styled.div.attrs({
     className: "ma3"
-})``;
+})`
+    ${mobile({ margin: "0px 20px", display: "flex", flexDirection: "column" })}
+
+`;
 
 const FilterText = styled.span.attrs({
     className: "f5 fw6 mr3"
-})``;
+})`
+    ${mobile({ marginRight: "0px" })}
+
+`;
 
 const Select = styled.select.attrs({
     className: "pa2 mr3"
-})``;
+})`
+    ${mobile({ margin: "10px 0px" })}
+`;
 
 const Option = styled.option``;
 
