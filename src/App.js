3ec91dce448ad8673f1d55e811af8745b1fa015e
techbrain2 <techbrain203@outlook.com>
2022-09-04 12:04:39 -0400

    added cart, login, register route

--- src/App.js
+++ src/App.js
@@ -4,14 +4,24 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import ProductItem from "./pages/ProductItem";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
+  const user = true;
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<ProductList />} />
         <Route path="/product/:id" element={<ProductItem />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={
+        user ? 
+        <Login />
+        } />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
 
