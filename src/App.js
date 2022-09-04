fa4999ff562bc52e3b2cbf062e38bbf65498a693
techbrain2 <techbrain203@outlook.com>
2022-09-04 11:44:17 -0400

    added products route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 
 import "./App.css";
-import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />}>
-
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<ProductList />} />
       </Routes>
     </Router>
 
