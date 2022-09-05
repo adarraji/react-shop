47a6dbb3b925cf2da469c8b000d6bb91175a71aa
techbrain2 <techbrain203@outlook.com>
2022-09-05 15:28:10 -0400

    get products data with category

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -13,8 +13,7 @@ const Products = ({ cat, filters, sort }) => {
     useEffect(() => {
         const getProducts = async () => {
             try {
-                const res = await axios.get("http://localhost:5000/api/products");
-                console.log(res)
+                const res = await axios.get(cat ? `http://localhost:5000/api/products?category=${cat}` : "http://localhost:5000/api/products");
             } catch (err) {
 
             }
