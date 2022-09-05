76ccd9af461f642b81785a1adff5eb7c09236660
techbrain2 <techbrain203@outlook.com>
2022-09-05 15:29:19 -0400

    update products state after receiving data

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -14,6 +14,7 @@ const Products = ({ cat, filters, sort }) => {
         const getProducts = async () => {
             try {
                 const res = await axios.get(cat ? `http://localhost:5000/api/products?category=${cat}` : "http://localhost:5000/api/products");
+                setProducts(res.data);
             } catch (err) {
 
             }
