6b6069e40542498a9654b65e2b290b91f689bab5
techbrain2 <techbrain203@outlook.com>
2022-09-05 15:21:11 -0400

    fix

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,13 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { popularProducts } from "../../data";
 import Product from "./Product";
 import styled from "styled-components";
+import axios from "axios";
 
 
 const Products = ({ cat, filters, sort }) => {
 
     const [products, setProducts] = useState([]);
     const [filteredProducts, setFilteredProducts] = useState([]);
+
+    useEffect(() => {
+        const getProducts = async () => {
+            try {
+                const res = await axios.get("http://localhost:5000/api/products");
+                console.log(res)
+            } catch (err) {
+
+            }
+        }
+        getProducts();
+    }, [cat])
 
     return (
         <Container className="pa4 flex justify-between flex-wrap">
