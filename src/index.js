43d3f58f20a174c05f3066c91db5c04ac2e9e8ea
Ali Darraji <techbrain203@outlook.com>
2022-09-16 11:13:44 -0400

    persist reducer configuration

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "tachyons";
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 import { Provider } from "react-redux";
+import { PersistGate } from 'redux-persist/integration/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
-  </Provider>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider >
 );
 
 // If you want to start measuring performance in your app, pass a function
