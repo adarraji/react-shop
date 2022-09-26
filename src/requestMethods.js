7d6f41ff01a3868a7ef79192689ef60c5998d066
adarraji <a.darraji@outlook.com>
2022-09-26 16:46:55 -0400

    add temp token

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
+const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMzIwZWU0NmQ1MTcxZDI2YzY0NTNhZCIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2NjQyMjUwNDAsImV4cCI6MTY2NDQ4NDI0MH0.3kDpLIl4mMVXTmmYfabzFUDi0o91MnBcneQgouZEqFs";
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
